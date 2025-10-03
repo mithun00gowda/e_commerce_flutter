3b68a26728cebec31297c76ddf4d1ff215af604e
Mithun M. <mithun@avani.local>
2025-10-03 18:53:12 +0530

    chapter 2

--- lib/constants/theme_data.dart
+++ lib/constants/theme_data.dart
@@ -5,7 +5,8 @@ class Styles{
   static ThemeData themeData({required bool isDarkTheme, required BuildContext context}){
     return ThemeData(
       scaffoldBackgroundColor: isDarkTheme ? AppColors.darkScaffoldColor : AppColors.lightScaffoldColor,
-      cardColor: isDarkTheme ? Color.fromARGB(255, 13, 6, 37) : AppColors.lightCardcolor
+      cardColor: isDarkTheme ? Color.fromARGB(255, 13, 6, 37) : AppColors.lightCardcolor,
+      brightness: isDarkTheme ? Brightness.dark : Brightness.light
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:e_commerce/provider/theme_provider.dart';
+import 'package:e_commerce/widgets/title_text.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -15,10 +16,8 @@ class HomeScreen extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          const Text(
-            'Hello World!',
-            style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-          ),
+          //TODO ADD TEXT
+           TitleText(),
           ElevatedButton(onPressed: () {}, child: const Text('Click Me')),
           SwitchListTile(
               title: Text(themeProvider.getIsDarkTheme ? "Dark Mode" : "Light Mode"),
